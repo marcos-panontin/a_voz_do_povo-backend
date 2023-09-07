6bcc882bea3b9f5753b787cddab2ea751c3444d2
Marcos Panontin <amarcospanontin@gmail.com>
2023-09-07 18:47:02 -0300

    implements Quiz Results

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 const cors = require('cors');
 const path = require('path');
 const answersRouter = require('./src/routes/answersRouter');
